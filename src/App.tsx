5db0eca954f6f75ab7151184cf97c854a7b9f9aa
marioValent <valent_mario97@yahoo.com>
2022-07-13 00:06:34 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+interface Image {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 function App() {
+  const [imgData, setImageData] = useState<Image[]>([]);
+  const [currImgList, setcurrImgList] = useState<JSX.Element[]>([]);
+  const [inputValue, setInputValue] = useState<number>(-1);
+  const [btnClicked, setBtnClicked] = useState<boolean>(false);
+
+  // const imageContainer = useRef(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageData(data as Image[]);
+      });
+  }, []);
+
+  const onBtnClick = (): void => {
+    const imgList: JSX.Element[] = [];
+    setBtnClicked(true);
+    for (let i: number = 0; i < imgData.length; i++) {
+      const imgObj: Image = imgData[i];
+      if (inputValue === imgObj.albumId) {
+        imgList.push(<img src={imgObj.url} alt={imgObj.title} />);
+      }
+    }
+    setcurrImgList(imgList);
+  };
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    setInputValue(parseInt(e.target.value));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Show Images from Album</h1>
+      <input
+        onChange={onInputChange}
+        className="input-number"
+        placeholder="here goes the id"
+      ></input>
+      <button onClick={onBtnClick} id="button">
+        Show image from chosen album
+      </button>
+      <div id="image-container">
+        {currImgList.length === 0 && btnClicked
+          ? "album inexistent"
+          : currImgList}
+      </div>
     </div>
   );
 }
